0dd2c79ba51b378b90f3e463434afad080bd3081
SagarSangle97github <132348294+SagarSangle97github@users.noreply.github.com>
2023-05-14 00:57:34 +0530

    Add files via upload

---
+++ CrudWithWebApiNet/CrudWithWebApiNet/Model/Brand.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudWithWebApiNet.Model
+{
+    [Table("Brand")]
+    public class Brand
+    {
+        [Key]
+        public int Id { get; set; }
+        /*  public object ID { get; internal set; }*/
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+
+        public int IsActive { get; set; }
+    }
+}
